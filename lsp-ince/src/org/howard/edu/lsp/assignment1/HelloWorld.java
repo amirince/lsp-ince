be389afb9fc46bf06cb3eb17c8f8e6a55ef7102f
incea <incea@10.26.98.135>
2022-08-31 12:26:22 -0400

    Commit for assignment 1

---
+++ lsp-ince/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Amir Ince
+ * @author incea
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
